bb2662300fb212985fea71299ed510174b0d0c88
Sebastián Fuentes <81325525+GreyTen1@users.noreply.github.com>
2025-05-22 00:00:00 -0300

    cambios

---
+++ include/clsCuota.h
@@ -0,0 +1,18 @@
+#ifndef CLSCUOTA_H_INCLUDED
+#define CLSCUOTA_H_INCLUDED
+
+#include "clsSocio.h"
+#include "clsFecha.h"
+
+class Cuota{
+
+    private:
+    Fecha fecha(int d, int m, int a);
+
+    public:
+
+
+};
+
+
+#endif // CLSCUOTA_H_INCLUDED
