8a093e7a23f967fde6c7108a92195fa65e29e862
Sayver206 <santinogerevini8@gmail.com>
2025-06-04 08:32:04 -0300

    Update clsImporteExporte.h

--- include/clsImporteExporte.h
+++ include/clsImporteExporte.h
@@ -1,6 +1,10 @@
 #ifndef CLSIMPORTEEXPORTE_H_INCLUDED
 #define CLSIMPORTEEXPORTE_H_INCLUDED
 
-
+class ImporteExporte {
+public:
+    void exportarCuotasATxt(const char *nombreBinario, const char *nombreTxt);
+};
 
 #endif // CLSIMPORTEEXPORTE_H_INCLUDED
+
