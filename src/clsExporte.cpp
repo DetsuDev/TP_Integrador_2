ab00632dba07ab53b921a85f42c0cbf05fc59715
Sebastián Fuentes <81325525+GreyTen1@users.noreply.github.com>
2025-06-11 19:42:50 -0300

    mas correcciones xd

--- src/clsExporte.cpp
+++ src/clsExporte.cpp
@@ -14,7 +14,7 @@ void Exporte::archExportar(std::string claseCls)
         std::ofstream file("CSV/Socios.csv");
 
         int cantReg = arcSoc.contarRegistros();
-        file << "DNI" << "\n";
+        file << "DNI" << "," <<"NOMBRE " << "\n";
         for(int i=0; i<cantReg; i++)
         {
             obj = arcSoc.leerRegistro(i);
