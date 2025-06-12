e4bbb2fdb6ae4f25ceed8f6e932c75aab77d1415
Sayver206 <santinogerevini8@gmail.com>
2025-06-12 09:07:52 -0300

    Create clsBackup.h
    Backup Beta

---
+++ include/clsBackup.h
@@ -0,0 +1,10 @@
+//#ifndef CLSBACKUP_H
+//#define CLSBACKUP_H
+
+//class BackupManager {
+//public:
+  //  bool hacerBackup(const char* archivoOriginal, const char* archivoBackup);
+    //void backupGeneral();
+//};
+
+// #endif // CLSBACKUP_H
