db9a7111494528e2168d2097848c8365bd90aab0
Sayver206 <santinogerevini8@gmail.com>
2025-06-13 17:15:05 -0300

    Update clsBackup.h
    Agregando restauración

--- include/clsBackup.h
+++ include/clsBackup.h
@@ -5,6 +5,9 @@ class BackupManager {
 public:
     bool hacerBackup(const char* archivoOriginal, const char* archivoBackup);
     void backupGeneral();
+
+    bool restaurarArchivo(const char* archivoBackup, const char* archivoOriginal); // NUEVO
+    void restaurarGeneral(); // NUEVO
 };
 
- #endif // CLSBACKUP_H
+#endif // CLSBACKUP_H
